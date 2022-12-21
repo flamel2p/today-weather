726f530590582f08e82387b94baa2ab710cc1750
flame <flamelimzhipeng@gmail.com>
2022-12-21 18:34:41 +0800

    update deploy

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import { Method, req, setBodyToUrlParam } from './core';
 import { GeocodingWeatherDataQuery, GeocodingWeatherData } from '../types/geocoding-weather-data';
 
-const apiHost: string = 'http://api.openweathermap.org';
+const apiHost: string = 'https://api.openweathermap.org';
 const apiKey: string = '3bd21b173100bab19e4335cd0fdb6720';
 
 export const fetchGeocodingWeatherData = (body: GeocodingWeatherDataQuery) =>
